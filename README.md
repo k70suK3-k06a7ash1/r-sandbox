da5a23004af57c7aa04634895281e536c1f9943a
2814109 <2814109k@gmail.com>
2024-11-18 06:18:55 +0900

    chore

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # r-sandbox
 
+## install libs
+
+```bash
+R
+
+> install.packages("prophet")
+```
+
 ## exec
 
 ```bash
 Rscript my_script.R
+Rscript sample.R
 ```
 
 ## REPL
--- sample.r
+++ sample.r
@@ -1,1 +1,14 @@
-rnorm(20)
+
+# R
+library(prophet)
+
+df <- read.csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
+m <- prophet(df)
+future <- make_future_dataframe(m, periods = 365)
+tail(future)
+
+forecast <- predict(m, future)
+tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
+
+plot(m, forecast)
+
